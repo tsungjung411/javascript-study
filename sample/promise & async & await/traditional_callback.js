6d5b844993fbffe9a66740e6604eb7347eaea7e0
Tsung-Jung Tsai (TJ_Tsai) <tsungjung411@yahoo.com.tw>
2019-06-11 11:04:18 +0800

    Create traditional_callback.js

---
+++ sample/promise & async & await/traditional_callback.js
@@ -0,0 +1,24 @@
+// 成功後要回傳呼叫的函數(Callback)，這樣才可以讓呼叫者知道狀況
+function successCallback(result) {
+    console.log("成功: 結果:", result);
+}
+
+// 失敗後要回傳呼叫的函數(Callback)，這樣才可以讓呼叫者知道狀況
+function failureCallback(error) {
+    console.log("失敗: 錯誤訊息:", error);
+}
+
+function doSomething(successCallback, failureCallback) {
+    // 隨機產生 true(50%) 或 false(50%)
+    var trueOfFalse = parseInt(Math.floor(Math.random() * 2));
+    if (trueOfFalse) {
+        successCallback('Done');
+    } else {
+        failureCallback('(T＿T)');
+    }
+}
+
+// 呼叫三次
+doSomething(successCallback, failureCallback)
+doSomething(successCallback, failureCallback)
+doSomething(successCallback, failureCallback)
