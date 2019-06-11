6eb3d06bae79ce6266c6e211e4151867c1388fae
Tsung-Jung Tsai (TJ_Tsai) <tsungjung411@yahoo.com.tw>
2019-06-11 11:06:52 +0800

    Update traditional_callback.js

--- sample/promise & async & await/traditional_callback.js
+++ sample/promise & async & await/traditional_callback.js
@@ -24,3 +24,4 @@ function doSomething(successCallback, failureCallback) {
 doSomething(successCallback, failureCallback)
 doSomething(successCallback, failureCallback)
 doSomething(successCallback, failureCallback)
+
