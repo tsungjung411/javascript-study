a4b9d62a38ce4eee17deb5df92dfd4bafc17ab05
Tsung-Jung Tsai (TJ_Tsai) <tsungjung411@yahoo.com.tw>
2019-06-11 11:05:42 +0800

    Update traditional_callback.js

--- sample/promise & async & await/traditional_callback.js
+++ sample/promise & async & await/traditional_callback.js
@@ -1,3 +1,4 @@
+
 // 成功後要回傳呼叫的函數(Callback)，這樣才可以讓呼叫者知道狀況
 function successCallback(result) {
     console.log("成功: 結果:", result);
@@ -8,6 +9,7 @@ function failureCallback(error) {
     console.log("失敗: 錯誤訊息:", error);
 }
 
+// 做某些事
 function doSomething(successCallback, failureCallback) {
     // 隨機產生 true(50%) 或 false(50%)
     var trueOfFalse = parseInt(Math.floor(Math.random() * 2));
